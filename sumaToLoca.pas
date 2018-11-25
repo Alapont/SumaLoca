fc7501d63aac5be0a0b3791cd6ad96126132bfb1
Pablo Alapont <pablo_alapont@hotmail.com>
2018-11-25 01:13:29 +0100

    inicio de la suma to loca

---
+++ sumaToLoca.pas
@@ -0,0 +1,39 @@
+program SumaLoca;
+label 1;
+const
+	Gilipollas = true;
+
+
+procedure sucesor(i integer)
+const
+
+procedure disimular(tiempo : integer)
+
+const
+	plank = 6.62607004E-34
+var
+	var i:integer;
+begin
+	max:=tiempo;
+	i:=0;
+	1:repeat
+	if (i<>max)then
+		begin
+			i+1i:=sucesor(i);
+		end;
+end.
+
+procedure ganarConcurso;
+	{Procedimiento para ganar un concurso}
+const
+	pi = 3.14159268;
+var
+	sumando1,sumando2:integer;
+	gilipollas:Boolean;
+	mierda: String(20);
+begin
+	Writeln ('A ver, gilipollas. Quieres sumar números');
+	Read (mierda);
+	Writeln ('No era una pregunta');
+
+end.
